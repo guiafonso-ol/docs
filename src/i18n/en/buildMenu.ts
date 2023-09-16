161d7a875ac5e0d893bb7c7d6ba6d313eda249a8
hannah <109289700+hannah-sk@users.noreply.github.com>
2023-09-16 14:09:10 -0300

    build-menu

--- src/i18n/en/buildMenu.ts
+++ src/i18n/en/buildMenu.ts
@@ -15,26 +15,26 @@ export default [
 
     /// START HERE :::: DO NOT REMOVE the strings above, it's a work around for header on mobile /// 
     { text: 'Overview', header: true, anchor: true, slug: '/documentation/products/build/overview/', key: 'buildOverview', hasLabel: 'menu.build' },
-    { text: 'Build with Edge Applications', header: true, anchor: true, slug: '/documentation/products/build/build-with-edge-app/', key: 'buildEdgeApps' },
+    { text: 'Build with Edge Application', header: true, anchor: true, slug: '/documentation/products/build/build-with-edge-app/', key: 'buildEdgeApps' },
 
     {
         text: 'Edit your edge application', header: true, type: 'learn', key: 'editEdgeApp',  children: [
             { text: 'Configure main settings', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/configure-main-settings/', key: 'mainSettings' },
             { text: 'Create device groups', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/create-device-groups/', key: 'deviceGroups' },
-            { text: 'Set error pages', header: true, anchor: true, type: 'learn', slug: 'errorPages', key: 'errorPages' },
+            { text: 'Set error pages', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/set-error-pages/', key: 'errorPages' },
             { text: 'Work with origins', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/work-with-origins/', key: 'origins' },
-            { text: 'Tune your cache settings', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/tune-cache-settings/', key: 'cacheSettings' },
-            { text: 'Work with edge functions', header: true, anchor: true, type: 'learn', slug: 'rules', key: 'rules' },
-            { text: 'Instantiate an edge function', header: true, anchor: true, type: 'learn', slug: 'functions', key: 'functions' },
+            { text: 'Tune cache settings', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/tune-cache-settings/', key: 'cacheSettings' },
+            { text: 'Work with rules engine', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/work-with-rules-engine/', key: 'rules' },
+            { text: 'Instantiate an edge function', header: true, anchor: true, type: 'learn', slug: '/documentation/products/guides/build/instantiate-edge-functions/', key: 'functions' },
         ]
     },
 
 
-    { text: 'Save and Deploy', header: true, anchor: true, type: 'learn', slug: 'saveDeploy', key: 'saveDeploy' },
+    { text: 'Save and deploy WIP', header: true, anchor: true, type: 'learn', slug: '/documentation/', key: 'saveDeploy' },
 
     {
-        text: 'Advanced configuration', header: true, type: 'learn', key: 'advancedConfig',addBorder: true, children: [
-            { text: 'Process Images', header: true, anchor: true, type: 'learn', slug: 'processImages', key: 'processImages', hasLabel: 'menu.buildAdvanced' },
+        text: 'Advanced configurations', header: true, type: 'learn', key: 'advancedConfig',addBorder: true, children: [
+            { text: 'Process images', header: true, anchor: true, type: 'learn', slug: 'processImages', key: 'processImages', hasLabel: 'menu.buildAdvanced' },
             { text: 'Multi-origin', header: true, anchor: true, type: 'learn', slug: 'multiOrigin', key: 'multiOrigin' },
             { text: 'Live Streaming', header: true, anchor: true, type: 'learn', slug: 'liveStreaming', key: 'streaming' },
         ]
@@ -44,7 +44,7 @@ export default [
 
     { text: 'Overview', header: true, anchor: true, slug: '/documentation/products/build/develop-with-azion/overview/', key: 'developOverview', hasLabel: 'menu.buildDev' },
     {
-        text: 'Developer Tools', header: true, type: 'learn', key: 'devToolsBuild', children: [
+        text: 'Developer tools', header: true, type: 'learn', key: 'devToolsBuild', children: [
             { text: 'CLI', slug: '/documentation/products/build/develop-with-azion/cli/', key: 'developCli' },
             { text: 'Azion IDE', slug: '/documentation/products/build/develop-with-azion/code-editor/', key: 'developIDE' },
             { text: 'Local Development', slug: '/documentation/products/build/develop-with-azion/local-dev/', key: 'developLocalDev' },
@@ -60,11 +60,12 @@ export default [
             { text: 'Overview', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/overview/', key: 'frameworkAngular' },
             { text: 'Angular', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/angular', key: 'frameworkAngular' },
             { text: 'Next.js', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/next', key: 'frameworkNext' },
-            { text: 'Nuxt', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/nuxt', key: 'frameworkNuxt' },
+            { text: 'Nuxt WIP', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/nuxt', key: 'frameworkNuxt' },
             { text: 'Vue', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/vue', key: 'frameworkVue' },
             { text: 'Astro', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/astro', key: 'frameworkAstro' },
             { text: 'Vite', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/vite', key: 'frameworkVite' },
             { text: 'React', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/react', key: 'frameworkReact' },
+            { text: 'Hexo', slug: '/documentation/products/build/develop-with-azion/frameworks-specific/hexo/', key: 'frameworkHexo' },
 
 
         ]
@@ -80,18 +81,8 @@ export default [
     { text: 'Environment variables', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/develop-with-azion/environment-variables/', key: 'envVars', hasLabel: 'menu.buildData', addBorder: true, },
 
 
-    { text: 'Understand Metrics', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/troubleshooting/understanding-metrics/', key: 'understandMetrics', hasLabel: 'menu.buildTroubleshoot' },
-    { text: 'Debugging and Application', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/troubleshooting/debug-applications/', key: 'debugApps',addBorder: true, },
-
-
-    { text: 'Tutorials', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/overview/', key: 'advConfig', addBorder: true, hasLabel: 'menu.buildTutorials' },
-
-
-    { text: 'Guides', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/overview/', key: 'advConfig', addBorder: true, hasLabel: 'menu.buildGuides' },
-
-
-    { text: 'Learn more', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/overview/', key: 'advConfig', addBorder: true, hasLabel: 'menu.buildLearn' },
-
+    { text: 'Understand metrics', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/troubleshooting/understanding-metrics/', key: 'understandMetrics', hasLabel: 'menu.buildTroubleshoot' },
+    { text: 'Debug applications', header: true, anchor: true, type: 'learn', slug: '/documentation/products/build/troubleshooting/debug-applications/', key: 'debugApps',addBorder: true, },
 
 
 ] as const;
