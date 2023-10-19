c6c83dcecf272dad3ec63d8a6f1058599f36b472
Luis Igreja <lfsigreja@gmail.com>
2023-10-19 17:18:40 -0300

    FIX: Same page links + button external

--- plugins/rehypeLinks.mjs
+++ plugins/rehypeLinks.mjs
@@ -10,6 +10,7 @@ const setLinkAttributes = (node) => {
   if (node.tagName === "p" || node.tagName === "ul" || node.tagName === "li" || node.tagName === "h2" || node.tagName === "h3" ||  node.tagName === "aside" ||  node.tagName === "section") {
     node.children.forEach((n) => setLinkAttributes(n))
   } else if (node.tagName === "a") {
+		if (node.properties.href.startsWith('#')) return
     node.properties.target = "_blank"
     node.properties.rel = "noopener"
   }
